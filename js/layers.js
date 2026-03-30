74e5e1d53abd2ed7fd4b1f480a46f8baf031241e
NhojNhojNhoj <johnwightman3@gmail.com>
2026-03-30 19:20:31 +1100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -14,7 +14,8 @@ addLayer("p", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+        let mult = new Decimal(1)
+        if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -36,7 +37,7 @@ addLayer("p", {
     description: "More Bonus Points = More Gain",
     cost: new Decimal(5),
     effect() {
-        return player[this.layer].points.add(1).pow(0.5)
+    return player[this.layer].points.add(1).pow(0.5)
     },
     effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
     },
@@ -45,7 +46,7 @@ addLayer("p", {
     description: "More Bonus Points = More Gain",
     cost: new Decimal(25),
     effect() {
-        return player[this.layer].points.add(1).pow(0.5)
+        return player[this.layer].points.add(1).pow(1.5)
     },
     effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
     },
